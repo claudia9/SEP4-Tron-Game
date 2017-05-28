081cb41ff75aab5acf81508f58f8654939a7cf54
Claudia Nou Carrera <claudianou@gmail.com>
2017-05-28 22:51:58 +0200

    - Now working properly both tasks. - TODO: Make processingMatrix to run with proper semaphore synchronization.

--- FreeRTOS-Test/main.c
+++ FreeRTOS-Test/main.c
@@ -26,70 +26,16 @@
 
 static const uint8_t _COM_RX_QUEUE_LENGTH = 30;
 
-static QueueHandle_t _received_chars_queue = NULL;	//CNC 20172505
+//Our variables
+static QueueHandle_t _received_chars_queue = NULL;
 static SemaphoreHandle_t  xMutexReceivedData = NULL;
-static bool run_process_matrix = false;
+
+static int start_game = 1;
+static int run_process_matrix = 0;
 
 // frame_buf contains a bit pattern for each column in the display
 static uint16_t frame_buf[14] = {0, 0, 28, 62, 126, 254, 508, 254, 126, 62, 28, 0, 0, 0};
 
-
-//-----------------------------------------
-/*void another_task(void *pvParameters)
-{
-// The parameters are not used
-( void ) pvParameters;
-
-#if (configUSE_APPLICATION_TASK_TAG == 1)
-// Set task no to be used for tracing with R2R-Network
-vTaskSetApplicationTaskTag( NULL, ( void * ) 2 );
-#endif
-
-BaseType_t result = 0;
-uint8_t byte;
-
-while(1)
-{
-result = xQueueReceive(_x_com_received_chars_queue, &byte, 1000L);
-
-if (result) {
-com_send_bytes(&byte, 1);
-}else {
-com_send_bytes((uint8_t*)"TO", 2);
-}
-}
-}*/
-
-
-//-----------------------------------------
-/*void startup_task(void *pvParameters)
-{
-// The parameters are not used
-( void ) pvParameters;
-
-#if (configUSE_APPLICATION_TASK_TAG == 1)
-// Set task no to be used for tracing with R2R-Network
-vTaskSetApplicationTaskTag( NULL, ( void * ) 1 );
-#endif
-
-_x_com_received_chars_queue = xQueueCreate( _COM_RX_QUEUE_LENGTH, ( unsigned portBASE_TYPE ) sizeof( uint8_t ) );
-init_com(_x_com_received_chars_queue);
-
-// Initialise Mutex
-xMutex = xSemaphoreCreateMutex();
-
-// Initialization of tasks etc. can be done here
-BaseType_t t1 = xTaskCreate(another_task, (const char *)"Another", configMINIMAL_STACK_SIZE, (void *)NULL, tskIDLE_PRIORITY, NULL);
-
-
-// Lets send a start message to the console
-com_send_bytes((uint8_t *)"Then we Start!\n", 15);
-
-while(1)
-{
-vTaskDelay( 1000 );
-}
-}*/
 
 
 bool correctArrowKey(char data_received[])
@@ -107,85 +53,93 @@ bool correctArrowKey(char data_received[])
 		default  : return false;
 
 	}
-	
 }
-
-/************************************************************************/
-/* CNC 20172505                                                         */
-/************************************************************************/
-
-void waitForHello(void *pvParameters){
-
-	//The parameters are not used
-	( void ) pvParameters;
-
-	#if (configUSE_APPLICATION_TASK_TAG == 1)
-	// Set task no to be used for tracing with R2R-Network
-	vTaskSetApplicationTaskTag( NULL, ( void * ) 1 );
-	#endif
-	
-
-	//Variables
-	//BaseType_t received_data = 0;
-	
-	uint8_t data[] = "";
-	uint8_t response[] = { 0x4f, 0x4b };	//OK
-
-	_received_chars_queue = xQueueCreate(_COM_RX_QUEUE_LENGTH, (unsigned portBASE_TYPE) sizeof (uint8_t));
-	init_com(_received_chars_queue);	//Initialize comPort at the board
-
-	vTaskDelay(500);
-	while (1) {
-
-		vTaskDelay(500);
-		//Check until we receive something - Waiting for a number 1.
-		if (xQueueReceive(_received_chars_queue, &data, ( TickType_t ) 10)) {
-			
-			com_send_bytes(data, 1);
-
-			if (data[0] == 49){		//If it's equal as 1
-				//Send first confirmation message to the console.
-				
-				com_send_bytes((uint8_t *)" Ready to receive - Reading...\n", 32);
-				
-				vTaskDelay(500);
-
-				while (1) {
-					if (xQueueReceive(_received_chars_queue, &data,  ( TickType_t ) 10)){
-						if (correctArrowKey(&data) == true) {
-							com_send_bytes((uint8_t *)" Key: ", 6);
-							com_send_bytes((uint8_t *)data, 1);
-							
-							xMutexReceivedData = xSemaphoreCreateMutex();
-							if (xSemaphoreTake(xMutexReceivedData, (TickType_t) 10 ) == pdTRUE) {
-								run_process_matrix = 1;
-								xSemaphoreGive(xMutexReceivedData);
-							} else if (data[0] == 32) {
-							com_send_bytes((uint8_t *)"Pause mode\n", 12);
-							} else {
-							com_send_bytes((uint8_t *)"Not a valid key\n", 16);
-							}
-						}
-					}
-				}
-			} else {
-			com_send_bytes((uint8_t *)"Not a valid key - Press 1 to play\n", 35);
-			vTaskDelay(500);
-			}
-		}
-	}
-}
-
 
 void processMatrix(void* pvParameters) {
 	
 	//The parameters are not used
 	( void ) pvParameters;
 	
-	if (run_process_matrix == 1) {
-		com_send_bytes((uint8_t *)"Processing matrix...!\n", 22);
+	while (1) {
+		//See if we can obtain the semaphore. If the semaphore is not available, wait 10 ticks to see if it becomes free.
+		if (xSemaphoreTake(xMutexReceivedData, (TickType_t) 0) == pdTRUE) {
+			//We were able to obtain the semaphore and can now access the shared resource.
+			com_send_bytes((uint8_t *)"Processing matrix\n", 19);
+			vTaskDelay(50);
+			
+			// We have finished accessing the shared resource. Release the semaphore.
+			xSemaphoreGive(xMutexReceivedData);
+
+		}
 	}
 }
+
+void waitForKeyPress(void *pvParameters){
+
+	//The parameters are not used
+	( void ) pvParameters;
+
+	//Variables
+	uint8_t data[] = "";
+	
+	_received_chars_queue = xQueueCreate(_COM_RX_QUEUE_LENGTH, (unsigned portBASE_TYPE) sizeof (uint8_t));
+	init_com(_received_chars_queue);	//Initialize comPort at the board
+
+
+	//Semaphore
+	xMutexReceivedData = xSemaphoreCreateMutex();
+	//Take semaphore before everything happens
+	//xSemaphoreTake(xMutexReceivedData, (TickType_t) 0);
+
+	#if (configUSE_APPLICATION_TASK_TAG == 1)
+	// Set task no to be used for tracing with R2R-Network
+	vTaskSetApplicationTaskTag( NULL, ( void * ) 1 );
+	#endif
+
+	while (start_game == 1) {
+		//Check until we receive something - Waiting for a number 1.
+		if (xQueueReceive(_received_chars_queue, &data, ( TickType_t ) 10)) {
+			com_send_bytes(data, 1);
+
+			if (data[0] == 49){		//If it's equal as 1
+				//Send first confirmation message to the console.
+				com_send_bytes((uint8_t *)" Ready to receive - Reading...\n", 32);
+				vTaskDelay(50);
+				start_game = 0;
+				} else {
+				com_send_bytes((uint8_t *)"Not a valid key - Press 1 to play\n", 35);
+				vTaskDelay(100);
+			}
+		}
+	}
+
+	init_com(_received_chars_queue);	//Initialize comPort at the board
+	
+	while (1) {
+		xSemaphoreTake(xMutexReceivedData, (TickType_t) 0 );
+		if (xQueueReceive(_received_chars_queue, &data,  ( TickType_t ) 10)){
+			if (correctArrowKey(&data) == true) {
+				com_send_bytes((uint8_t *)" Key: ", 6);
+				com_send_bytes((uint8_t *)data, 1);
+				
+				
+				//if (xSemaphoreTake(xMutexReceivedData, (TickType_t) 0 ) == pdTRUE) {
+				run_process_matrix = 1;
+				xSemaphoreGive(xMutexReceivedData);
+				//}
+				} else if (data[0] == 32) {
+				com_send_bytes((uint8_t *)"Pause mode\n", 12);
+				vTaskDelay(10);
+				} else {
+				com_send_bytes((uint8_t *)"Not a valid key\n", 16);
+				vTaskDelay(10);
+			}
+		}
+	}
+	BaseType_t taskProcessMatrix = xTaskCreate(processMatrix, (const char *)"Process Matrix", configMINIMAL_STACK_SIZE, (void *) NULL, tskIDLE_PRIORITY, NULL);
+}
+
+
 
 // Prepare shift register setting SER = 1
 void prepare_shiftregister()
@@ -241,7 +195,8 @@ void handle_display(void)
 	}
 }
 
-//-----------------------------------------
+
+//Don't delete or program crashes..
 void vApplicationIdleHook( void )
 {
 	//
@@ -250,29 +205,26 @@ void vApplicationIdleHook( void )
 //-----------------------------------------
 int main(void)
 {
-	
 	init_board();
 	
 	// Shift register Enable output (G=0)
 	PORTD &= ~_BV(PORTD6);
-	
-	//Create task to blink gpio
-	BaseType_t taskWaitForHello = xTaskCreate(waitForHello, (const char *)"Wait for hello", configMINIMAL_STACK_SIZE, (void *)NULL, tskIDLE_PRIORITY, NULL);
 
-	BaseType_t taskProcessMatrix = xTaskCreate(processMatrix, (const char *)"Process Matrix", configMINIMAL_STACK_SIZE, (void *) NULL, tskIDLE_PRIORITY, NULL);
+	//Create task to check key press
+	BaseType_t taskWaitForKeyPress = xTaskCreate(waitForKeyPress, (const char *)"Wait for key press", configMINIMAL_STACK_SIZE, (void *)NULL, tskIDLE_PRIORITY, NULL);
 
 	// Start the display handler timer
 	init_display_timer(handle_display);
 	
 	sei();
-	
+
 	//Start the scheduler
 	vTaskStartScheduler();
-	
+
 	//Should never reach here
 	while (1)
 	{
 	}
+	
 }
-
 
